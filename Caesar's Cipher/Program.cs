d2a2c4bbea74472403d213c07495415549c770b0
th3worst4 <caiocouto25@hotmail.com>
2022-03-24 04:56:03 -0300

    Changing code to VS Code

--- Caesar's Cipher/Program.cs
+++ Caesar's Cipher/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Caesar_s_Cipher
 {
-    internal class Program
+    static class Program
     {
         static void Main(string[] args)
         {
